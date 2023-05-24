eb15157e91169db0fadfe95fdef4e5060c5bdd8a
take_miyazaki <take_miyazaki@figinc.jp>
2023-05-24 12:06:18 +0900

    2nd commit

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import random
 from speech import Speech
 
 if __name__ == "__main__":
-    # sp1 = Speech("./wav/", tts_mode=True)
-    sp2 = Speech("./wav/", tts_mode=False)
+    # sp = Speech("./wav/", tts_mode=True)
+    sp = Speech("./wav/", tts_mode=False)
     count = 1
     while (True):
         count += 1
-        # sp1.talk(1, "いちばん", 0.7, 12, 5.0)
-        # sp1.talk(2, "にばん", 0.7, 12, 5.0)
-        # sp1.talk(3, "さんばん", 0.7, 12, 5.0)
+        # sp.talk(1, "いちばん", 0.7, 12, 5.0)
+        # sp.talk(2, "にばん", 0.7, 12, 5.0)
+        # sp.talk(3, "さんばん", 0.7, 12, 5.0)
 
         target_index = random.randint(1, 3)
-        sp2.talk(index=target_index, limit_sec=5.0)
+        sp.talk(index=target_index, limit_sec=3.0)
         print(f"index={target_index}")
+
         if count == 3:
             count = 1
         time.sleep(1.0)
--- speech.py
+++ speech.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 import time
-from collections import deque
 from threading import Thread
 
 
@@ -12,12 +11,9 @@ class Speech(object):
         '''
         self.tts_mode = tts_mode
         self.wavdir = wavdir
-        self.fname_list = []
-        self.playdt_dict = {}
+        self.fname = None
         self.playdt = 0
-        self.proc_dict = {}
-        self.queue = deque()
-        self.reset()
+        self.proc = None
 
         self.thread_active_flg = True
         self.thread = Thread(target=self.__talk_thread, args=())
@@ -48,11 +44,10 @@ class Speech(object):
         WAVファイル文字列から必要な音声合成パラメータを取り出す
         '''
         arr = fname.split('_')
-        index = int(arr[0])
         speed_rate = int(arr[1]) / 100.0
         volume_db = int(arr[2]) / 100.0
         text = arr[3]
-        return index, speed_rate, volume_db, text
+        return speed_rate, volume_db, text
 
     def __get_tts_command(self, speed_rate, volume_db):
         '''
@@ -87,81 +82,45 @@ class Speech(object):
     def __talk_thread(self):
         while self.thread_active_flg:
             try:
-                if 0 < len(self.queue):
-                    fname = self.queue.popleft()
-                    # self.playdt_dict[fname] = time.perf_counter()
+                if self.fname is not None:
+                    fname = self.fname
+                    self.fname = None
                     self.playdt = time.perf_counter()
 
                     # 音声合成モードの場合, テキスト情報に基づきWAVを生成する
                     if self.tts_mode:
-                        index, speed_rate, volume_db, text = self.__get_tts_info(
+                        speed_rate, volume_db, text = self.__get_tts_info(
                             fname)
                         if self.__generate_wav(
-                            fname, speed_rate, volume_db, text):
+                                fname, speed_rate, volume_db, text):
                             continue
-                        print(f"play {text}")
-                    else:
-                        print(f"play {fname}")
 
-                    # 全てのWAV再生インスタンスをKillする
-                    for tmp_fname in self.fname_list:
-                        if self.proc_dict[tmp_fname] is not None:
-                            self.proc_dict[tmp_fname].kill()
+                    # # WAV再生インスタンスをKillする
+                    if self.proc is not None:
+                        self.proc.kill()
 
                     # WAV再生実行
                     aplay_cmd = ['aplay', '-q',  '%s%s.wav' %
                                  (os.path.expanduser(self.wavdir), fname)]
-                    self.proc_dict[fname] = subprocess.Popen(aplay_cmd,
-                                                             shell=False,
-                                                             stdout=subprocess.PIPE)
-                else:
-                    time.sleep(0.1)
+                    print(f"play {fname}.wav")
+                    self.proc = subprocess.Popen(
+                        aplay_cmd, shell=False, stdout=subprocess.PIPE)
+
             except Exception as e:
                 print("exception at speech.talk_thread() : %s" % (e))
-
-    def reset(self):
-        self.queue.clear()
-        for fname in self.fname_list:
-            self.playdt_dict[fname] = 0
-            self.playdt = 0
-            self.proc_dict[fname] = None
+            finally:
+                time.sleep(0.1)
 
     def talk(self, index=0, text="", speed_rate=1.0, volume_db=12, limit_sec=1.0):
         try:
-            if limit_sec <= 0:
-                self.queue.clear()
+            if self.fname is not None:
+                return
 
             target_fname = self.__get_fname(index, text, speed_rate, volume_db)
 
-            is_exist = False
-            for fname in self.fname_list:
-                if fname == target_fname:
-                    is_exist = True
-                    break
-
-            if is_exist == False:
-                # 新規音声出力の場合, リストに追加しておく
-                self.fname_list.append(target_fname)
-                self.playdt_dict[target_fname] = 0
-                self.proc_dict[target_fname] = None
-                self.queue.append(target_fname)
-
             # 指定秒数を超えた場合, 発音許可する
-            # elif limit_sec <= 0 or limit_sec <= (time.perf_counter() - self.playdt_dict[target_fname]):
-            elif limit_sec <= 0 or (0 < self.playdt and limit_sec <= (time.perf_counter() - self.playdt)):
-
-                # 全ての再生インスタンスをKillする
-                for fname in self.fname_list:
-                    if self.proc_dict[fname] is not None:
-                        try:
-                            self.proc_dict[fname].kill()
-                        except:
-                            pass
-                        finally:
-                            self.proc_dict[fname] = None
-
-                self.queue.append(target_fname)
+            if limit_sec <= 0 or (limit_sec <= (time.perf_counter() - self.playdt)):
+                self.fname = target_fname
 
         except Exception as e:
             print("exception at talk() : %s" % (e))
-
